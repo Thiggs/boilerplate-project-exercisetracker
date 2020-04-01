e26ed91d7f88caff7d7bd3112315d3fdfb4b98f6
Tyler <realpsych22@gmail.com>
2020-04-01 14:26:45 -0500

    add new user, if unique

--- server.js
+++ server.js
@@ -2,22 +2,101 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongo = require('mongodb').MongoClient;
-
+const shortid = require('shortid')
 const cors = require('cors')
-
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/exercise-track' )
 
 app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+mongoose.connect(process.env.MONGO_URI, function (err) {
+   if (err) console.error(err);
+});
+
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  username: { type: String, required: true, default: shortid.generate},
+  exercise: String,
+  duration: String,
+  date: {}
+});
+
+//Create new username if unique
+var Person = mongoose.model("Person", personSchema);
+
+var createPerson = (name, done)=>{
+  Person.findOne({username: name}, (err, searchData)=>{
+    if(searchData) {
+      done(null, "used");
+      }
+    else if (err){
+        done(err);
+      }
+    else {
+      const person = new Person({
+        username: name,
+        exercise: "",
+        duration: "",
+        date: {}
+        })
+        person.save((err,data)=>{
+          if(err){
+            done(err);
+          }
+          done(null , data);
+        });
+};
+  });
+};
+
+app.post("/api/exercise/new-user", (req, res)=>{
+  createPerson(req.body.username, (err, nameData)=>{
+    if (nameData === "used"){
+      res.send({"error": "username already taken"});
+    }
+    else if(err){
+      res.send({error: "error"});
+    }
+  else
+  res.send({"username":nameData.username});
+  });
+});
+
+
+
+ 
+
+//I can get an array of all users by getting api/exercise/users with the same info as when creating a user.
+
+//Add Exercises
+
+  //if required data is not entered return Path `duration`/`description is required.
+
+  //return object containing the username, exercise, duration, and date (default to today)
+
+
+  //you can retrieve all of a users data from GET users's exercise log: GET /api/exercise/log?{userId}[&from][&to][&limit]
+
+//{ } = required, [ ] = optional
+
+//from, to = dates (yyyy-mm-dd); limit = number
+
+
+
+
+
+
+
+
 
 
 // Not found middleware
